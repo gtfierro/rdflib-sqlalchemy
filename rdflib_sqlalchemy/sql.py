4d0b80226281650dd22419ed88ec25b3cdd4e9a2
Gabe Fierro <gtfierro@mines.edu>
2026-09-24 22:58:18 -0600

    Fix count query on SQLAlchemy 2.1
    SQLAlchemy 2.1 no longer accepts a Select in select_from(), which broke
    len() on every store. Wrap the inner select in .subquery(), which also
    works on 2.0.
    
    (cherry picked from commit 3083ddbc765cbb46510be4033345fdd3bf1bbc93)

--- rdflib_sqlalchemy/sql.py
+++ rdflib_sqlalchemy/sql.py
@@ -63,7 +63,7 @@ def union_select(select_components, distinct=False, select_type=TRIPLE_SELECT):
             else:
                 raise ValueError('Unrecognized table type {}'.format(tableType))
             select_clause = expression.select(*[functions.count().label('aCount')]).select_from(
-                expression.select(*cols).where(whereClause).distinct().select_from(table))
+                expression.select(*cols).where(whereClause).distinct().select_from(table).subquery())
         elif select_type == CONTEXT_SELECT:
             select_clause = expression.select(table.c.context)
             if whereClause is not None:
